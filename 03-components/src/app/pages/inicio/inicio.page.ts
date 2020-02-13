1bfef5d245906dbfbef90254a84f134e995c09f5
Raiser93 <brayan_933@hotmail.com>
2020-02-13 10:54:46 -0500

    Tabulacion

--- 03-components/src/app/pages/inicio/inicio.page.ts
+++ 03-components/src/app/pages/inicio/inicio.page.ts
@@ -1,29 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.page.html',
-  styleUrls: ['./inicio.page.scss'],
+    selector: 'app-inicio',
+    templateUrl: './inicio.page.html',
+    styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
 
-    components: ComponentsRoutes[] = [
-    {
-      icon: 'american-football',
-      name: 'Action Sheet',
-      redirectTo: '/action-sheet'
-    },
-    {
-      icon: 'logo-google-playstore',
-      name: 'Alert',
-      redirectTo: '/alert'
-    },
-  ];
+    components: ComponentsRoutes[] = [{
+            icon: 'american-football',
+            name: 'Action Sheet',
+            redirectTo: '/action-sheet'
+        },
+        {
+            icon: 'logo-google-playstore',
+            name: 'Alert',
+            redirectTo: '/alert'
+        },
+    ];
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {}
 
 }
 
